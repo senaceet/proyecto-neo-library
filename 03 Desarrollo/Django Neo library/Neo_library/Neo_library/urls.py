942f780e007999c1b4430e3ce1f719b8327d0473
carlos andres ruiz <caruiz269@misena.edu.co>
2021-06-01 19:35:06 -0500

    added: static files

--- 03 Desarrollo/Django Neo library/Neo_library/Neo_library/urls.py
+++ 03 Desarrollo/Django Neo library/Neo_library/Neo_library/urls.py
@@ -5,5 +5,12 @@ from prestamos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('log_admin',views.log_admin,name='log_admin'),
+    path('login_user',views.log_user,name='log_user'),
+    path('administracion',views.administration,name='administration'),
+    path('mis_prestamos',views.my_loans,name='mis_prestamos'),
+    path('prestamos',views.loans,name='prestamos'),
+    path('users',views.users,name='usuarios'),
+    path('books',views.books,name='libros')
 ]
--- 03 Desarrollo/Django Neo library/Neo_library/prestamos/views.py
+++ 03 Desarrollo/Django Neo library/Neo_library/prestamos/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 
 def index (request):
     return render(request,'index.html')
+def log_admin (request):
+    return render(request,'loginad.html')
+def log_user (request):
+    return render(request,'loginus.html')
+def administration (request):
+    return render (request,'administracion.html')
+def books (request):
+    return render (request,'libros.html')
+def my_loans (request):
+    return render (request,'mis_prestamos.html')
+def loans (request):
+    return  render (request,'prestamos.html')
+def users (request):
+    return render (request,'usuarios.html')
