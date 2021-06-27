2c802bae3fd3d6f1806b2438aac79f25618613c8
carlos andres ruiz <caruiz269@misena.edu.co>
2021-06-26 19:48:59 -0500

    added: log in tests

--- 03 Desarrollo/Django Neo library/Neo_library/prestamos/tests/test_views.py
+++ 03 Desarrollo/Django Neo library/Neo_library/prestamos/tests/test_views.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from django.contrib.auth.models import User
+
 class testviews (TestCase):
+
+    def setUp(self):
+        #Create two users
+        test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        test_user1.save()
+        
+
 
     def test_index_view_correct (self):
         resp = self.client.get(reverse('index'))
@@ -50,3 +59,28 @@ class testviews (TestCase):
         self.assertEqual(resp.status_code, 200)
 
         self.assertTemplateUsed(resp, 'libros.html')
+
+    
+    def test_logged_in_uses_correct_template_log_Admin(self):
+        login = self.client.login(username='testuser1', password='12345')
+        resp = self.client.get(reverse('log_admin'))
+
+        #Check our user is logged in
+        self.assertEqual(str(resp.context['user']), 'testuser1')
+        #Check that we got a response "success"
+        self.assertEqual(resp.status_code, 200)
+
+        #Check we used correct template
+        self.assertTemplateUsed(resp, 'loginad.html')
+
+    def test_logged_in_uses_correct_template_log_user(self):
+        login = self.client.login(username='testuser1', password='12345')
+        resp = self.client.get(reverse('log_user'))
+
+        #Check our user is logged in
+        self.assertEqual(str(resp.context['user']), 'testuser1')
+        #Check that we got a response "success"
+        self.assertEqual(resp.status_code, 200)
+
+        #Check we used correct template
+        self.assertTemplateUsed(resp, 'loginus.html')
